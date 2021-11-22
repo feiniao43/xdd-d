ad1acd6c97acc490b57b88b6b4d05f9231320b72
feiniao43 <85214867+feiniao43@users.noreply.github.com>
2021-11-22 09:46:15 +0800

    Update asset.go

--- models/asset.go
+++ models/asset.go
@@ -97,7 +97,21 @@ func (ck *JdCookie) Query2() string {
 	}
 	return msg
 }
-
+func (ck *JdCookie) Query3() string {
+	name:="jd_all_bean_change.js"
+	envs:=[]Env{{Name:"pins",Value:"&"+ck.PtPin}}
+	msg:=runTask(&Task{Path:name,Envs:envs},&Sender{})
+	//log.Info(msg)
+	if !strings.Contains(msg,"cookies"){
+		msg=regexp.MustCompile(`^(.+\s+){3}|\s*.+\s*$|.*东东工厂.*\s*`).ReplaceAllString(msg,"")
+		msg=fmt.Sprintf("京东账号用户名：%s\n京东账号昵称：%s\n绑定QQ: %v\n用户等级：%v\n等级名称：%v\n更新时间: %s\n%s",ck.PtPin,ck.Nickname,ck.QQ,ck.UserLevel,ck.LevelName,ck.CreateAt,msg)
+	}else if CookieOK(ck){
+		msg=fmt.Sprintf("查询失败\n京东账号用户名：%s\n京东账号昵称：%s\n备注: %s\n%s",ck.PtPin,ck.Nickname,ck.Note,msg)
+	}else{
+		msg=fmt.Sprintf("失效账号\n京东账号用户名：%s\n京东账号昵称：%s备注: %s",ck.PtPin,ck.Nickname,ck.Note)
+	}
+	return msg
+}
 
 func (ck *JdCookie) Query() string {
 	msgs := []string{
